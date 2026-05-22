4aebb20fd4e65e48b9248cb5a340080968a612b1
Safdar  Ali <148275412+Safdar307@users.noreply.github.com>
2026-05-23 01:28:46 +0500

    Add files via upload

---
+++ loops/generate_any_number_table.php
@@ -0,0 +1,29 @@
+<?php
+
+//using for loop
+
+
+$num = 15;
+
+for($a = 1; $a<=10; $a++){
+   echo $num . " * " . $a . " = " . ($num * $a);
+   echo "<br/>";
+}
+
+
+
+//using while loop
+
+/*
+$num = 10;
+$a = 1;
+
+while($a <= 10){
+    echo $num . " * " . $a . " = " . ($num * $a);
+    echo "<br/>";
+    $a++;
+}
+*/
+
+
+?>
