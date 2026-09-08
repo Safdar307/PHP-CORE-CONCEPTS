1ac9973d859e510c7ff1078ccc0db0e0c0cea869
Safdar  Ali <148275412+Safdar307@users.noreply.github.com>
2026-09-08 22:43:00 +0500

    Add files via upload

---
+++ OOPS/encapsulation.php
@@ -0,0 +1,119 @@
+<!-- Encapsulation, we protect our data in encapsulation, private and protected, we protect data by getter and setter. -->
+
+
+<?php
+
+// Encapsulation
+
+
+// class User{
+//     private $name;
+
+//     public function __construct($name){
+//         $this->name = $name;
+//     }
+
+//     public function setName($setName){
+//         $this->name = $setName;
+//     }
+
+//     public function getName(){
+//         return $this->name;
+//     }
+// }
+
+// $user1 = new user("Safdar Ali");
+
+// echo  $user1->getNAme();
+
+// $user1->setName("Ahmed Ali");
+
+// echo  $user1->getNAme();
+
+
+
+
+// class User{
+//     private $name;
+
+//     public function __construct($name){
+//         $this->name = $name;
+//     }
+
+//     public function setName($setName){
+//         if (strlen($setName) >= 6){
+//         $this->name = $setName;
+//         }
+//         else {
+//             echo "Name is not valid";
+//         }
+//     }
+
+//     public function getName(){
+//         return $this->name;
+//     }
+// }
+
+// $user1 = new user("Safdar Ali");
+
+// echo  $user1->getName();
+
+// $user1->setName("Ah");
+
+// echo  $user1->getNAme();
+
+
+
+
+// class User{
+//     private $name;
+
+//     public function __construct($name){
+//         $this->name = $name;
+//     }
+
+//     public function setName($setName){
+//         if (strlen($setName) >= 6){
+//         $this->name = $setName;
+//         return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+
+//     public function getName(){
+//         return $this->name;
+//     }
+// }
+
+// $user1 = new user("Safdar Ali");
+
+// echo  $user1->getName();
+// echo "<br>";
+
+// if ($user1->setName("Ahmed Ali")){
+//     echo "Name updated Successfully";
+// }else{
+//     echo "Invalid name";
+// }
+
+// echo "<br>";
+// echo $user1->getName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
